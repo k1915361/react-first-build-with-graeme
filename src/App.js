b74199efe824d15d3f271f037894ad2e6d6dc0f9
k1915361 <67165821+k1915361@users.noreply.github.com>
2022-06-07 13:54:08 +0100

    Added route(r/s), NavLink and Link react-router-dom components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import Layout from './components/layouts/Layout';
 import Home from './components/pages/Home';
 import SignIn from './components/pages/SignIn';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContactUs from './components/pages/ContactUs';
 import PageNotFound from './components/pages/404';
 
 import './App.css';
 
+// 1:20:56
+
 function App() {
   return (
+    <BrowserRouter>
     <Layout>
-      <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/contact' element={<ContactUs />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
     </Layout>
+    </BrowserRouter>
   );
 }
 
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,13 +1,16 @@
 import './Header.css';
 import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
       <header>
-          <a>
+          <Link to="/">
             <img src="https://img.icons8.com/ios-filled/50/000000/conference-call.png" alt="Icon showing group" />
-          </a>
-          <h1>React First Build</h1>
+          </Link>
+          <Link to="/">
+            <h1>React First Build</h1>
+          </Link>
           <div className="login">
             <p>Welcome Eugene!</p>
           </div>
